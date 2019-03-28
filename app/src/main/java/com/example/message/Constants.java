b3ad0e6dccc1b62dea4e21fe06359d1193debe0a
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-03-28 21:40:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/message/Constants.java
@@ -0,0 +1,5 @@
+package com.example.message;
+
+public class Constants {
+    public static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 100;
+}
---
+++ app/src/main/java/com/example/message/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.message;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    Button btnSend;
+    EditText tvMessage;
+    EditText tvNumber;
+    IntentFilter intentFilter;
+
+
+    private BroadcastReceiver intentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView inTxt = (TextView) findViewById(R.id.textMsg);
+            inTxt.setText(intent.getExtras().getString("message"));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        Constants.MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+            }
+        }
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SMS_RECEIVED_ACTION");
+
+        btnSend = (Button) findViewById(R.id.btnSend);
+        tvMessage = (EditText) findViewById(R.id.tvMessage);
+        tvNumber = (EditText) findViewById(R.id.tvNumber);
+
+        btnSend.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myMsg = tvMessage.getText().toString();
+                String theNumber = tvNumber.getText().toString();
+                sendMsg(theNumber, myMsg);
+            }
+        });
+    }
+
+    protected void sendMsg(String theNumber, String myMsg) {
+        String SENT = "Message Sent";
+        String DELIVERED = "Message Delivered";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(theNumber, null, myMsg, sentPI, deliveredPI);
+    }
+
+    @Override
+    protected void onResume() {
+        registerReceiver(intentReceiver, intentFilter);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(intentReceiver);
+        super.onPause();
+    }
+}
